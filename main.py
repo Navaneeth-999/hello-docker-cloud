eed250810aa7b00c60000b86537ba07bd43ae60f
Navaneeth <navaneethkadiyam@gmail.com>
2026-01-30 20:43:29 +0530

    add pymongo and mongodb connection

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+from pymongo import MongoClient
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = MongoClient(MONGODB_URI)
+db = client["bug_tracker_db"]
+
 from fastapi import FastAPI
 
 app = FastAPI(
